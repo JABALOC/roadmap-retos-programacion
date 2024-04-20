13949027e134bd1ae8f64a832760a87cf98f1247
Jabaloc <167613721+JABALOC@users.noreply.github.com>
2024-04-20 12:51:58 +0200

    Create Jabaloc.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Jabaloc.java
@@ -0,0 +1,32 @@
+//LA URL DEL SITIO OFICIAL DE JAVA ES: java.com
+
+//ESTO ES UN COMENTARIO DE UNA LÍNEA
+
+/*
+ESTO ES UN COMENTARIO 
+DE VARIAS LÍNEAS
+*/
+
+public class Jabaloc{
+
+  public static void main(String[]args){
+
+    //CREAMOS UNA VARIABLE Y UNA CONSTANTE:
+
+    String variable="Esto es una variable";
+    final String constante= "Esto es una constante";
+
+    //AHORA CREAMOS VARIABLES CON TODOS LOS TIPOS DE DATOS PRIMITIVOS:
+
+    boolean bool=true;
+    char letra='a';
+    byte b=8;
+    short s=16;
+    int i=32;
+    long l=64;
+    float f=3.14f;
+    decimal d=3.1416;
+
+    System.out.println("Hola java");
+  }
+}
